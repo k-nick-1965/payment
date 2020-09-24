b312628376c64a05d9e55421fbedc3d9f540e9f3
k-nick-1965 <k-nick@yandex.ru>
2020-09-24 23:01:26 +0300

    24.09.2020 22:47 Null заменен на Optional.empty()... Не проникся...

--- src/main/java/ru/sbrf/payment/common/exchange/ExchWithClient.java
+++ src/main/java/ru/sbrf/payment/common/exchange/ExchWithClient.java
@@ -1,10 +1,11 @@
 package ru.sbrf.payment.common.exchange;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public interface ExchWithClient {
 
-    <T extends Container> T GetFromTheClient(Class<T> valueType) throws ContainerExeption, ClassNotFoundException;
+    <T extends Container> Optional<T> GetFromTheClient(Class<T> valueType) throws ContainerExeption, ClassNotFoundException;
 
     void SendToClient(Container cont) throws IOException;
 
